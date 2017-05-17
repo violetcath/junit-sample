d0490ef6ddc805035e5ea951a59f82bfa21e389d
Bianca Denise del Puerto <biancadenise12@users.noreply.github.com>
2017-05-17 23:50:36 +0800

    Update SampleTest.java

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -10,12 +10,12 @@ public class SampleTest {
 		int b = 2;
 		assertTrue(a + b == 3);
 	}
-	/*
+	
  	@Test
  	public void simpleTestFailed() {
  		int a = 1;
  		int b = 2;
  		assertTrue(a + b == 4);
  	}
-	*/
+	
 }
